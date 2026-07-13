ee18502ce8012f546ed79b427ebb310f8ff4ca0d
Moroni Motta <moroni.art.motta@gmail.com>
2026-07-12 17:35:21 -0700

    user auth done

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connection successful");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+const PORT = process.env.PORT || 3536;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
